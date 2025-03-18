77903ec6b1bd69d1aefebd2fb6ba29205928dcc6
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2025-03-17 22:01:41 -0300

    Update git-clone-comfyui.sh

--- git-clone-comfyui.sh
+++ git-clone-comfyui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /workspace/
-git clone git@github.com:comfyanonymous/ComfyUI.git
+git clone https://github.com/comfyanonymous/ComfyUI.git
 cd /workspace/ComfyUI/custom_nodes
 git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
