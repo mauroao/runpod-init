7f324b735d2a646115e1ae62e28cc50595959fe8
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2025-03-30 16:35:03 -0300

    .

--- diffusion-pipe/install.sh
+++ diffusion-pipe/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cd /workspace/diffusion-pipe/
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade wheel
+python3 -m pip install --upgrade setuptools
 pip install -r requirements.txt
 
 pip install -U "huggingface_hub[cli]"
