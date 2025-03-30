e22bdc3a4435649648460dc19f6c0df0e5fc2b89
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2025-03-29 22:49:22 -0300

    .

--- diffusion-pipe/clone.sh
+++ diffusion-pipe/clone.sh
@@ -4,4 +4,5 @@ cd /workspace/
 rm -rf diffusion-pipe
 git clone --recurse-submodules https://github.com/tdrussell/diffusion-pipe
 mv /workspace/diffusion-pipe/examples/dataset.toml /workspace/diffusion-pipe/examples/dataset.toml.old
-
+mkdir /workspace/diffusion-pipe/dataset
+cp /workspace/runpod-init/diffusion-pipe/dataset.toml /workspace/diffusion-pipe/examples/dataset.toml
