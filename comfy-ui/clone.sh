97a90217d51da5a73cc7128de179d9d8bdffb943
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2025-04-30 09:18:46 -0300

    .

--- comfy-ui/clone.sh
+++ comfy-ui/clone.sh
@@ -4,3 +4,5 @@ cd /workspace/
 git clone https://github.com/comfyanonymous/ComfyUI.git
 cd /workspace/ComfyUI/custom_nodes
 git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
+git clone https://github.com/Fannovel16/ComfyUI-Frame-Interpolation.git comfyui-frame-interpolation
+git clone https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite.git comfyui-videohelpersuite
