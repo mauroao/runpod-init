c09b1e195fa574d0a997fe89391152fc5bab7ae1
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2025-05-01 16:06:53 -0300

    .

--- comfy-ui/install.sh
+++ comfy-ui/install.sh
@@ -6,7 +6,11 @@ apt install -y ffmpeg
 pip install -r requirements.txt
 pip install opencv-python
 
+pip install -r /workspace/ComfyUI/custom_nodes/comfyui-wanvideowrapper/requirements.txt
+pip install -r /workspace/ComfyUI/custom_nodes/comfyui-videohelpersuite/requirements.txt
+pip install -r /workspace/ComfyUI/custom_nodes/comfyui-kjnodes/requirements.txt
 
-
+pip install triton
+pip install sageattention==1.0.6
 
 
