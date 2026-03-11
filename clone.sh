5fb3768aed5bf4a3695ae3cfd640bf3cc733c3f9
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2026-03-11 17:42:14 -0300

    remove hardcoded /comfy/runpod-init/

--- clone.sh
+++ clone.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 trap 'echo "ERROR: Command failed at line $LINENO with exit code $?" >&2; exit 1' ERR
 
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+
 cd /comfy/
 
 if [ -d "ComfyUI" ]; then
@@ -24,14 +26,14 @@ git clone https://github.com/yolain/ComfyUI-Easy-Use
 #git clone https://github.com/wallish77/wlsh_nodes
 
 mkdir -p /comfy/ComfyUI/user/default/workflows
-for f in /comfy/runpod-init/workflows/*.json; do
+for f in "$SCRIPT_DIR"/workflows/*.json; do
     ln -sf "$f" /comfy/ComfyUI/user/default/workflows/
 done
 
-ln -sf /comfy/runpod-init/files/comfy.settings.json /comfy/ComfyUI/user/default/comfy.settings.json
+ln -sf "$SCRIPT_DIR/files/comfy.settings.json" /comfy/ComfyUI/user/default/comfy.settings.json
 
 # temporary fix from https://github.com/welltop-cn/ComfyUI-TeaCache/issues/178
-ln -sf /comfy/runpod-init/files/nodes.py /comfy/ComfyUI/custom_nodes/ComfyUI-TeaCache/nodes.py
+ln -sf "$SCRIPT_DIR/files/nodes.py" /comfy/ComfyUI/custom_nodes/ComfyUI-TeaCache/nodes.py
 
 mkdir -p /comfy/ComfyUI/custom_nodes/comfyui-frame-interpolation/ckpts/rife
 FILE="/comfy/ComfyUI/custom_nodes/comfyui-frame-interpolation/ckpts/rife/rife47.pth"
